185ed2802da7d36b68122922317fa75eccf6a071
Daniel Dai <daniel.dai.email@gmail.com>
2023-02-13 21:08:49 -0500

    mid port backup

--- protoc-pool.ps1
+++ protoc-pool.ps1
@@ -1,1 +1,2 @@
-protoc --plugin=protoc-gen-ts_proto=.\site-app\node_modules\.bin\protoc-gen-ts_proto.cmd --ts_proto_opt=esModuleInterop=true --ts_proto_out=./site-app/src/pool ./pool.v1.proto
+# protoc --plugin=protoc-gen-ts_proto=.\site-app\node_modules\.bin\protoc-gen-ts_proto.cmd --ts_proto_opt=esModuleInterop=true --ts_proto_out=./site-app/src/pool ./pool.v1.proto
+Copy ./pool.v1.proto ./desktop-app/src-tauri/src
