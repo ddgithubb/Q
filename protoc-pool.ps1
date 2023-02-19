8e408a3d3c64e4604afe3afd6f6e18ccf275629b
Daniel Dai <daniel.dai.email@gmail.com>
2023-02-19 15:17:16 -0500

    finished porting. UNTESTED

--- protoc-pool.ps1
+++ protoc-pool.ps1
@@ -1,2 +1,2 @@
-# protoc --plugin=protoc-gen-ts_proto=.\site-app\node_modules\.bin\protoc-gen-ts_proto.cmd --ts_proto_opt=esModuleInterop=true --ts_proto_out=./site-app/src/pool ./pool.v1.proto
 Copy ./pool.v1.proto ./desktop-app/src-tauri/src
+protoc --plugin=protoc-gen-ts_proto=.\site-app\node_modules\.bin\protoc-gen-ts_proto.cmd --ts_proto_opt=esModuleInterop=true --ts_proto_out=./desktop-app/src/types ./pool.v1.proto
--- protoc-sync-server.ps1
+++ protoc-sync-server.ps1
@@ -1,3 +1,3 @@
 protoc --go_out=./sync-server sync_server.v1.proto
-# protoc --plugin=protoc-gen-ts_proto=.\site-app\node_modules\.bin\protoc-gen-ts_proto.cmd --ts_proto_opt=esModuleInterop=true --ts_proto_out=./site-app/src/pool ./sync_server.v1.proto
 Copy ./sync_server.v1.proto ./desktop-app/src-tauri/src
+protoc --plugin=protoc-gen-ts_proto=.\site-app\node_modules\.bin\protoc-gen-ts_proto.cmd --ts_proto_opt=esModuleInterop=true --ts_proto_out=./desktop-app/src/types ./sync_server.v1.proto
